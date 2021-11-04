400d28fcc572e7ae68487107ae582fc9146bae5e
Alex Bohza <bogza.alex2004@gmail.com>
2021-11-04 15:47:28 +0200

    First

---
+++ лаба2.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+#include "windows.h"
+using namespace std;
+int main()
+{
+	int x = 2;
+	double rad, s, p, pi = 3.14;
+
+	/*printf("Enter rad: ");
+	scanf_s("%lf", &rad);
+	s = pi * rad * rad;
+	p = x * pi * rad;
+	printf("s = %lf\n", s);
+	printf("p = %lf\n", p);*/
+
+	cout << "Enter rad: ";
+	cin >> rad;
+	s = pi * rad * rad;
+	p = x * pi * rad;
+	cout << pi << " * " << rad << " * " << rad << " = " << s <<endl;
+	cout << x << " * " << pi << " * " << rad << " = " << p << endl;
+}
