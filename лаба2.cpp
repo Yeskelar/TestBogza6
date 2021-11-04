719d9f08941cb974839953c50db3b4d4d73c329e
Alex Bohza <bogza.alex2004@gmail.com>
2021-11-04 16:00:56 +0200

    DELETED PART OF CODE

--- лаба2.cpp
+++ лаба2.cpp
@@ -1,4 +1,4 @@
-﻿
+
 
 #include <iostream>
 #include "windows.h"
@@ -7,14 +7,6 @@ int main()
 {
 	int x = 2;
 	double rad, s, p, pi = 3.14;
-
-	/*printf("Enter rad: ");
-	scanf_s("%lf", &rad);
-	s = pi * rad * rad;
-	p = x * pi * rad;
-	printf("s = %lf\n", s);
-	printf("p = %lf\n", p);*/
-
 	cout << "Enter rad: ";
 	cin >> rad;
 	s = pi * rad * rad;
